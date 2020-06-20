5f02df3392272cdfca81c92984d9c1c349c06605
Steven Black <genseb7@gmail.com>
2020-06-20 15:14:53 -0400

    alphabtz vars

--- variables.tf
+++ variables.tf
@@ -2,16 +2,16 @@
 # Variables
 ########################################
 
+variable "aws_lb_internal" {
+  description = "The Load Balancer which fronts the ASG is internal"
+  type        = bool
+  default     = true
+}
+
 variable "batch_max_size" {
   description = "Maximum batch size for ASG rolling updates"
   type        = string
   default     = 1
-}
-
-variable "health_check_type" {
-  description = "Check instance health with EC2 or ELB checks"
-  type        = string
-  default     = "EC2"
 }
 
 variable "health_check_path" {
@@ -26,6 +26,12 @@ variable "health_check_port" {
   default     = 80
 }
 
+variable "health_check_type" {
+  description = "Check instance health with EC2 or ELB checks"
+  type        = string
+  default     = "EC2"
+}
+
 variable "image_id" {
   description = "AMI ID to use for this cluster"
   type        = string
@@ -35,6 +41,22 @@ variable "instance_type" {
   description = "Instance type to use in ASG"
   type        = string
   default     = "t3.micro"
+}
+
+variable "instance_security_groups" {
+  description = "Security groups to apply to instances in ASG"
+  type        = list(string)
+}
+
+variable "lb_listener_port" {
+  description = "Port on which LB will listen"
+  type        = number
+  default     = 80
+}
+
+variable "lb_security_groups" {
+  description = "Security groups to apply to Load Balancer"
+  type        = list(string)
 }
 
 variable "max_instances" {
@@ -60,34 +82,12 @@ variable "subnet_ids" {
   type        = list(string)
 }
 
-variable "vpc_id" {
-  description = "VPC ID to use for target group"
-  type        = string
-}
-
-variable "lb_listener_port" {
-  description = "Port on which LB will listen"
-  type        = number
-  default     = 80
-}
-
-variable "aws_lb_internal" {
-  description = "The Load Balancer which fronts the ASG is internal"
-  type        = bool
-  default     = true
-}
-
-variable "instance_security_groups" {
-  description = "Security groups to apply to instances in ASG"
-  type        = list(string)
-}
-
-variable "lb_security_groups" {
-  description = "Security groups to apply to Load Balancer"
-  type        = list(string)
-}
-
 variable "tags" {
   description = "Tags definition to apply to resources"
   type        = map(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID to use for target group"
+  type        = string
 }
